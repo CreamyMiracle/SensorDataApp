e246e1b1f01a07d680a56346199c8f11512c3bb7
Juhana Rajala <juhana.rajala@vertex.fi>
2022-03-06 14:00:07 +0200

    Add SQLite

---
+++ SensorDataApp/SensorDataApp/Controllers/SensorDataController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using SensorDataApp.Service;
+using SensorDataApp.Hubs;
+using SensorDataApp.Model;
+
+namespace SensorDataApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SensorDataController : ControllerBase
+    {
+        private readonly ILogger<SensorDataController> _logger;
+        private readonly IHubContext<SensorDataHub> _sensorDataHub;
+        private DataService _dataService;
+
+        public SensorDataController(ILogger<SensorDataController> logger, DataService dataService, IHubContext<SensorDataHub> sensorDataHub)
+        {
+            _logger = logger;
+            _dataService = dataService;
+            _sensorDataHub = sensorDataHub;
+        }
+
+        [HttpGet("{sensorId}")]
+        public async Task<IEnumerable<DataPoint>> Get(string sensorId, [FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
+        {
+            return await _dataService.GetSensorDataPoints(sensorId, fromDate, toDate);
+        }
+
+        [HttpPost]
+        public async Task<DataPoint?> Post(DataPoint datapoint)
+        {
+            try
+            {
+                await _dataService.GetSensor(datapoint.SensorId);
+            }
+            catch (Exception ex)
+            {
+                Sensor newSensor = new Sensor() { Id = datapoint.SensorId };
+                await _dataService.AddSensor(new Sensor() { Id = datapoint.SensorId });
+            }
+
+            DataPoint dp = await _dataService.InsertDataPoint(datapoint);
+            if (dp != null)
+            {
+                await _sensorDataHub.Clients.Group(dp.SensorId).SendAsync("ReceiveDataPoint", dp);
+            }
+            return dp;
+        }
+    }
+}
---
+++ SensorDataApp/SensorDataApp/Data/DataService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SensorDataApp.Model;
+using SQLite;
+using SQLiteNetExtensionsAsync.Extensions;
+
+namespace SensorDataApp.Service
+{
+    public class DataService
+    {
+        private readonly SQLiteAsyncConnection db_con;
+
+        public DataService(SQLiteAsyncConnection con)
+        {
+            db_con = con;
+        }
+
+        public async Task<IEnumerable<Sensor>> GetSensors()
+        {
+            return await db_con.GetAllWithChildrenAsync<Sensor>();
+        }
+
+        public async Task<Sensor> GetSensor(string sensorId)
+        {
+            return await db_con.GetAsync<Sensor>(sensorId);
+        }
+
+        public async Task<Sensor?> AddSensor(Sensor sensor)
+        {
+            return await db_con.InsertAsync(sensor) == 1 ? sensor : null;
+        }
+
+        public async Task<Sensor?> PutSensor(Sensor sensor)
+        {
+            return await db_con.UpdateAsync(sensor) == 1 ? sensor : null;
+        }
+
+        public async Task<IEnumerable<DataPoint>> GetSensorDataPoints(string sensorId, DateTime fromDate, DateTime toDate)
+        {
+            return await db_con.GetAllWithChildrenAsync<DataPoint>(dp => dp.SensorId == sensorId && dp.Timestamp > fromDate && dp.Timestamp <= toDate);
+        }
+
+        public async Task<DataPoint?> InsertDataPoint(DataPoint datapoint)
+        {
+            return await db_con.InsertAsync(datapoint) == 1 ? datapoint : null;
+        }
+
+        #region Private Methods
+
+        #endregion
+    }
+}
